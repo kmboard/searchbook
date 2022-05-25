b36c77d05f8fff4ad6a59cffc861d5650ab3ae57
Kija <kmboard1@yahoo.com>
2022-05-25 17:31:11 -0400

    working on client side

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { saveBook } from '../utils/API';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -65,7 +66,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { input: bookInput }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
